e03c32689c2a97c32818248823e42bcdb0804c80
mortizo <mortizo@ups.edu.ec>
2022-05-11 07:54:33 -0500

    Ventana con lista de Labels finalizada

--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana01/Principal030101.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana01/Principal030101.java
@@ -12,8 +12,12 @@ public class Principal030101 {
     
     public static void main(String [] args){
     
-        var v = new Ventana030101("Ventana 2022-05-10",800,600);
+        //var v = new Ventana030101("Ventana 2022-05-10",800,600);
+        //v.setVisible(true);
+        
+        var v = new Ventana030102("Ventana 2022-05-11");
         v.setVisible(true);
+        
         
     }
     
--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana01/Ventana030102.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana01/Ventana030102.java
@@ -4,10 +4,55 @@
  */
 package semana01;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author morti
  */
-public class Ventana030102 {
+public class Ventana030102 extends JFrame{
+
+    private JPanel jPanel1;
+    private List<JLabel> jLabelList;
     
+   public Ventana030102(String title) {
+        super(title);
+        this.setSize(800, 600);
+        this.setLocation(50, 100);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.iniciarComponentes();
+        this.setContentPane(this.jPanel1);
+    }
+
+   public void iniciarComponentes(){
+        this.iniciarPanel();
+        this.iniciarEtiquetas();
+   }
+   
+   public void iniciarPanel(){
+       this.jPanel1 = new JPanel();
+       this.jPanel1.setBackground(Color.YELLOW);
+       this.jPanel1.setBorder(BorderFactory.createTitledBorder("Panel Principal"));
+       this.setLayout(new FlowLayout());
+   }
+   
+   public void iniciarEtiquetas(){
+       this.jLabelList = new ArrayList<>();
+       this.jLabelList.add(new JLabel());
+       this.jLabelList.add(new JLabel());
+       this.jLabelList.add(new JLabel());
+       this.jLabelList.get(0).setBackground(Color.ORANGE);
+       this.jLabelList.get(0).setOpaque(true);
+       this.jLabelList.get(0).setText("Electrónica y automatización");
+       this.jPanel1.add(this.jLabelList.get(0));
+   }
+
+   
 }
