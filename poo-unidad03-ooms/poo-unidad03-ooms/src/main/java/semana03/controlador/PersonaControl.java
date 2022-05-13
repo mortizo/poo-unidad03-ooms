e6a7ef3a19a7870a5a7c2d27349834f8559b2ff9
mortizo <mortizo@ups.edu.ec>
2022-05-12 21:31:40 -0500

    Unidad 03 Semana 03 Ventanas con Listeners

--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana03/controlador/PersonaControl.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana03/controlador/PersonaControl.java
@@ -18,21 +18,24 @@ public class PersonaControl {
     
     private final PersonaServicio personaServicio = new PersonaServicio();
     
-    public Persona crear(int codigo, String nombre, String apellido,int year, 
-            int mes, int dia) {
-        LocalDate fechaNacimiento = LocalDate.of(year, mes, dia);
-        var persona = new Persona(codigo,nombre,apellido,fechaNacimiento);
+    public Persona crear(String [] params) {
+        var fechaNacimiento = LocalDate.of(Integer.valueOf(params[3]),
+                Integer.valueOf(params[4]), 
+                Integer.valueOf(params[5]));
+      
+        var persona = new Persona(Integer.valueOf(params[0]),params[1],
+                params[2],fechaNacimiento);
         this.personaServicio.crear(persona);
         return persona;
     }
-
+    
     public List<Persona> listar() {
         return this.personaServicio.listar();
     }
 
-    public Persona getByCodigo(int i) {
-        if(i>0)
-            return this.personaServicio.getByCodigo(i);
+    public Persona getByCodigo(Object i) {
+        if((int)i>0)
+            return this.personaServicio.getByCodigo((int)i);
         else
             return this.personaServicio.getByCodigo(0);
     }
--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana03/vista/PersonaVentana0303b.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana03/vista/PersonaVentana0303b.java
@@ -4,12 +4,14 @@
  */
 package semana03.vista;
 
-import semana02.*;
 import java.awt.Color;
+import semana02.*;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.BorderFactory;
@@ -29,7 +31,7 @@ import semana03.modelo.Persona;
  *
  * @author morti
  */
-public class PersonaVentana0303a extends JFrame implements ActionListener{
+public class PersonaVentana0303b extends JFrame implements ActionListener, KeyListener{
 
     private List<JPanel> jPanelList;
     private List<JLabel> jLabelList;
@@ -38,7 +40,7 @@ public class PersonaVentana0303a extends JFrame implements ActionListener{
     private PersonaControl personaControl;
 
     
-    public PersonaVentana0303a(String title) {
+    public PersonaVentana0303b(String title) {
         super(title);
         this.setSize(500, 600);
         this.setLocation(50, 100);
@@ -57,16 +59,24 @@ public class PersonaVentana0303a extends JFrame implements ActionListener{
        this.jPanelList.add(new JPanel());
        this.jPanelList.add(new JPanel());
        this.jPanelList.add(new JPanel());
-       
+       this.jPanelList.add(new JPanel());
+       this.jPanelList.add(new JPanel());
+              
       
        this.jPanelList.get(0).setBorder(BorderFactory.createTitledBorder("Panel Principal"));
-       this.jPanelList.get(0).setLayout(new GridLayout(6,1));
+       this.jPanelList.get(0).setLayout(new GridLayout(8,1));
        this.jPanelList.get(0).add(this.jPanelList.get(1));
        this.jPanelList.get(0).add(this.jPanelList.get(2));
        this.jPanelList.get(0).add(this.jPanelList.get(3));
        this.jPanelList.get(0).add(this.jPanelList.get(4));
        this.jPanelList.get(0).add(this.jPanelList.get(5));
        this.jPanelList.get(0).add(this.jPanelList.get(6));
+       this.jPanelList.get(6).setBackground(Color.WHITE);
+       this.jPanelList.get(0).add(this.jPanelList.get(7));
+       this.jPanelList.get(7).setBackground(Color.WHITE);
+       this.jPanelList.get(0).add(this.jPanelList.get(8));
+       this.jPanelList.get(8).setBackground(Color.WHITE);
+       
        
        this.iniciarEtiquetas();
        this.jPanelList.get(1).add(this.jLabelList.get(0));
@@ -74,6 +84,8 @@ public class PersonaVentana0303a extends JFrame implements ActionListener{
        this.jPanelList.get(3).add(this.jLabelList.get(2));
        this.jPanelList.get(4).add(this.jLabelList.get(3));
        this.jPanelList.get(6).add(this.jLabelList.get(4));
+       this.jPanelList.get(7).add(this.jLabelList.get(5));
+       this.jPanelList.get(8).add(this.jLabelList.get(6));
        
        
        this.iniciarCuadrosTexto();
@@ -99,12 +111,19 @@ public class PersonaVentana0303a extends JFrame implements ActionListener{
        this.jLabelList.add(new JLabel());
        this.jLabelList.add(new JLabel());
        this.jLabelList.add(new JLabel());
+       this.jLabelList.add(new JLabel());
+       this.jLabelList.add(new JLabel());
        
        this.jLabelList.get(0).setText("Ingrese el código: "); 
        this.jLabelList.get(1).setText("Ingrese los nombres: ");
        this.jLabelList.get(2).setText("Ingrese los apellidos: ");
        this.jLabelList.get(3).setText("Ingrese la fecha de nacimiento: AAAA/MM/DD");
-
+       this.jLabelList.get(4).setOpaque(true);
+       this.jLabelList.get(4).setBackground(Color.WHITE);   
+       this.jLabelList.get(5).setOpaque(true);
+       this.jLabelList.get(5).setBackground(Color.WHITE); 
+       this.jLabelList.get(6).setOpaque(true);
+       this.jLabelList.get(6).setBackground(Color.WHITE); 
    }
 
    public void iniciarCuadrosTexto(){
@@ -116,7 +135,10 @@ public class PersonaVentana0303a extends JFrame implements ActionListener{
        this.jtextFieldList.add(new JTextField());
        this.jtextFieldList.add(new JTextField());
        
-       
+       this.jtextFieldList.get(0).addKeyListener(this);
+       this.jtextFieldList.get(1).addKeyListener(this);
+       this.jtextFieldList.get(2).addKeyListener(this);
+              
        this.jtextFieldList.get(0).setColumns(5);
        this.jtextFieldList.get(1).setColumns(20);
        this.jtextFieldList.get(2).setColumns(20);
@@ -131,9 +153,9 @@ public class PersonaVentana0303a extends JFrame implements ActionListener{
        this.jButtonList.add(new JButton());
        
        this.jButtonList.get(0).setText("Registrar");
-       this.jButtonList.get(0).addActionListener(this);
-       
        this.jButtonList.get(1).setText("Mostrar");
+       
+       this.jButtonList.get(0).addActionListener(this);
        this.jButtonList.get(1).addActionListener(this);
        
    }
@@ -156,6 +178,24 @@ public class PersonaVentana0303a extends JFrame implements ActionListener{
             for(Persona p: this.personaControl.listar())
                 System.out.println(p.toString());  
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if(e.getSource().equals(this.jtextFieldList.get(0)))
+            this.jLabelList.get(4).setText("keyTyped: "+String.valueOf(e.getKeyChar()));
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getSource().equals(this.jtextFieldList.get(1)))
+            this.jLabelList.get(5).setText("keyPressed: "+String.valueOf(e.getKeyChar()));
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getSource().equals(this.jtextFieldList.get(2)))
+            this.jLabelList.get(6).setText("keyReleased: "+String.valueOf(e.getKeyChar()));
     }
    
    
