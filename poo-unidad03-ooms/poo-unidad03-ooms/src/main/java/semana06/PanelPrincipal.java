806d13d9377f4de224cf21eae40d1be0c742c320
mortizo <mortizo@ups.edu.ec>
2022-05-20 16:18:23 -0500

    Bandera implementando MouseListener

--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana06/PanelPrincipal.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/semana06/PanelPrincipal.java
@@ -6,13 +6,23 @@ package semana06;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 
 /**
  *
  * @author morti
  */
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements MouseListener {
+
+    private int posX=-1;
+    private int posY=-1;
+    
+    public PanelPrincipal() {
+        this.addMouseListener(this);
+    }
+    
     @Override
     public void paint(Graphics g) {
         super.paint(g); 
@@ -24,5 +34,31 @@ public class PanelPrincipal extends JPanel {
         g.fillRect(100, 250, 300, 50);
         g.setColor(Color.GRAY);
         g.fillRect(90, 90, 10, 400);
+        g.setColor(Color.GREEN);
+        if(this.posX!=-1 || this.posY!=-1)
+            g.fillOval(this.posX, this.posY, 10, 10);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        this.posX=e.getX();
+        this.posY=e.getY();
+        this.repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
 }
