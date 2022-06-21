5fe11c33bdf8e86b5bca6bf4bad1e04d0a2eeb89
UPS <UPS@DESKTOP-8FGL23A>
2022-06-21 08:31:51 -0500

    Formulario Colores

--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p60/semana06/PanelPrincipal1.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p60/semana06/PanelPrincipal1.java
@@ -13,17 +13,52 @@ import javax.swing.JPanel;
  * @author UPS
  */
 public class PanelPrincipal1 extends JPanel{
+    
+    private Color color1;
+    private Color color2;
+    private Color color3;
 
+    public PanelPrincipal1(Color color1, Color color2, Color color3) {
+        this.color1 = color1;
+        this.color2 = color2;
+        this.color3 = color3;
+    }
+    
+    
     @Override
     public void paint(Graphics g) {
         super.paint(g);     
-        g.setColor(Color.RED);
+        g.setColor(this.color1);
         g.drawLine(0, 0, 600, 600);
-        g.setColor(Color.GREEN);
+        g.setColor(this.color2);
         g.fillOval(0, 0, 300, 300);
-        g.setColor(Color.ORANGE);
+        g.setColor(this.color3);
         g.fillRect(300, 300, 200, 250);
     
+    }
+
+    public Color getColor1() {
+        return color1;
+    }
+
+    public void setColor1(Color color1) {
+        this.color1 = color1;
+    }
+
+    public Color getColor2() {
+        return color2;
+    }
+
+    public void setColor2(Color color2) {
+        this.color2 = color2;
+    }
+
+    public Color getColor3() {
+        return color3;
+    }
+
+    public void setColor3(Color color3) {
+        this.color3 = color3;
     }
     
     
--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p60/semana06/Ventana0306a.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p60/semana06/Ventana0306a.java
@@ -4,8 +4,10 @@
  */
 package p60.semana06;
 
+import java.awt.Color;
 import p60.semana06.PanelPrincipal1;
 import java.awt.HeadlessException;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 
 /**
@@ -16,12 +18,21 @@ public class Ventana0306a extends JFrame{
 
     //private PanelPrincipal panelPrincipal;
     private PanelPrincipal1 panelPrincipal;
+    private JComboBox comboColor1;
+    private JComboBox comboColor2;
+    private JComboBox comboColor3;
     
     public Ventana0306a(String title) throws HeadlessException {
         super(title);
         this.setSize(600, 600);
+        this.comboColor1 = new JComboBox();
+        this.comboColor1.addItem("Rojo");
+        this.comboColor1.addItem("Azul");
+        this.comboColor1.addItem("Verde");
+        
       //  this.panelPrincipal = new PanelPrincipal();
-        this.panelPrincipal = new PanelPrincipal1();        
+      
+        this.panelPrincipal = new PanelPrincipal1(Color.PINK,Color.CYAN,Color.GREEN);        
         this.setContentPane(this.panelPrincipal);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
