c392075da25035bd739136c283370eba9ebeb2fb
mortizo <mortizo@ups.edu.ec>
2022-11-24 11:29:54 -0500

    vista para jugador incompleta sin radioButton

--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p61/unidad03_02/vista/Ventana0302a.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p61/unidad03_02/vista/Ventana0302a.java
@@ -10,6 +10,7 @@ import java.awt.HeadlessException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,6 +27,8 @@ public class Ventana0302a extends JFrame{
     public List<JLabel> jLabelList;
     public List<JTextField> jTextFieldList;
     public List<JButton> jButtonList;
+    public List<JCheckBox> jCheckBoxList;
+    
     
 
     public Ventana0302a(String title) throws HeadlessException {
@@ -35,6 +38,8 @@ public class Ventana0302a extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.iniciarPanels();
         this.iniciarJTextFields();
+        this.iniciarJButtons();
+        this.iniciarJCheckBoxs();
         this.setContentPane(this.jPanelList.get(0));
         this.setVisible(true);
     }
@@ -45,19 +50,22 @@ public class Ventana0302a extends JFrame{
         this.jPanelList.add(new JPanel());
         this.jPanelList.add(new JPanel());
         this.jPanelList.add(new JPanel());
+        this.jPanelList.add(new JPanel());
+        this.jPanelList.add(new JPanel());
+        this.jPanelList.add(new JPanel());
+        this.jPanelList.add(new JPanel());
         
         
-        
-        this.jPanelList.get(0).setBackground(Color.yellow);
-        this.jPanelList.get(1).setBackground(Color.blue);
-        this.jPanelList.get(2).setBackground(Color.red);
-        
-        this.jPanelList.get(0).setLayout(new GridLayout(6,1));
+        this.jPanelList.get(0).setLayout(new GridLayout(7,1));
         this.jPanelList.get(0).add(this.jPanelList.get(1));
         this.jPanelList.get(0).add(this.jPanelList.get(2));
+        this.jPanelList.get(0).add(this.jPanelList.get(3));
+        this.jPanelList.get(0).add(this.jPanelList.get(4));
+        this.jPanelList.get(0).add(this.jPanelList.get(5));
+        this.jPanelList.get(0).add(this.jPanelList.get(7));
+        this.jPanelList.get(0).add(this.jPanelList.get(6));
         
-        
-        
+ 
     }
     
     public void iniciarJTextFields(){
@@ -85,20 +93,42 @@ public class Ventana0302a extends JFrame{
         this.jPanelList.get(1).add(this.jTextFieldList.get(0));
         this.jPanelList.get(2).add(this.jLabelList.get(1));
         this.jPanelList.get(2).add(this.jTextFieldList.get(1));
-    /*    this.jPanelList.get(0).add(this.jLabelList.get(2));
-        this.jPanelList.get(0).add(this.jTextFieldList.get(2));
-        this.jPanelList.get(0).add(this.jLabelList.get(3));
-        this.jPanelList.get(0).add(this.jTextFieldList.get(3));
-        this.jPanelList.get(0).add(this.jLabelList.get(4));
-        this.jPanelList.get(0).add(this.jTextFieldList.get(4));
-        this.jPanelList.get(0).add(this.jTextFieldList.get(5));
-        this.jPanelList.get(0).add(this.jTextFieldList.get(6));
-        */
+        this.jPanelList.get(3).add(this.jLabelList.get(2));
+        this.jPanelList.get(3).add(this.jTextFieldList.get(2));
+        this.jPanelList.get(4).add(this.jLabelList.get(3));
+        this.jPanelList.get(4).add(this.jTextFieldList.get(3));
+        this.jPanelList.get(5).add(this.jLabelList.get(4));
+        this.jPanelList.get(5).add(this.jTextFieldList.get(4));
+        this.jPanelList.get(5).add(this.jTextFieldList.get(5));
+        this.jPanelList.get(5).add(this.jTextFieldList.get(6));
+        
     
     }
     
     public void iniciarJButtons(){
         this.jButtonList = new ArrayList<>();
+        
+        this.jButtonList.add(new JButton("Guardar"));
+        this.jButtonList.add(new JButton("Cancelar"));
+        
+        this.jPanelList.get(6).add(this.jButtonList.get(0));
+        this.jPanelList.get(6).add(this.jButtonList.get(1));
+        
+        
+    }
+    
+    public void iniciarJCheckBoxs(){
+        this.jCheckBoxList = new ArrayList<>();
+        
+        this.jCheckBoxList.add(new JCheckBox("Arquero",true));
+        this.jCheckBoxList.add(new JCheckBox("Defensa"));
+        this.jCheckBoxList.add(new JCheckBox("Mediocampo"));
+        this.jCheckBoxList.add(new JCheckBox("Delantero"));
+        
+        this.jPanelList.get(7).add(this.jCheckBoxList.get(0));
+        this.jPanelList.get(7).add(this.jCheckBoxList.get(1));
+        this.jPanelList.get(7).add(this.jCheckBoxList.get(2));
+        this.jPanelList.get(7).add(this.jCheckBoxList.get(3));
         
     }
     
