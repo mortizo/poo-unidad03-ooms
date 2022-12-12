ffd4a3adae7ac7e66cdecb11f0b5d75e191565df
mortizo <mortizo@ups.edu.ec>
2022-12-12 15:03:14 -0500

    Se agrega combos a Viaje y JInternal Frame

--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p61/unidad03_05/controlador/ViajeControl.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p61/unidad03_05/controlador/ViajeControl.java
@@ -2,14 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package p61.unidad03_04.controlador;
+package p61.unidad03_05.controlador;
+
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
-import p61.unidad03_04.servicio.ViajeServiceImpl;
-import p61.unidad03_04.modelo.Viaje;
-import p61.unidad03_04.servicio.CiudadServiceImpl;
+import p61.unidad03_05.servicio.ViajeServiceImpl;
+import p61.unidad03_05.modelo.Viaje;
+import p61.unidad03_05.servicio.CiudadServiceImpl;
 
 /**
  *
