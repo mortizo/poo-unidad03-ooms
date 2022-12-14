92dbd15f18b9208a338e16c45eabda4069394f7d
mortizo <mortizo@ups.edu.ec>
2022-12-14 11:31:07 -0500

    Menus y MenuItem

--- poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p61/unidad03_05/vista/VentanaPrincipal.java
+++ poo-unidad03-ooms/poo-unidad03-ooms/src/main/java/p61/unidad03_05/vista/VentanaPrincipal.java
@@ -4,6 +4,8 @@
  */
 package p61.unidad03_05.vista;
 
+import javax.swing.JFrame;
+
 /**
  *
  * @author morti
@@ -15,12 +17,13 @@ public class VentanaPrincipal extends javax.swing.JFrame {
      */
     public VentanaPrincipal() {
         initComponents();
+        /*
         var ventanaCiudad = new VentanaCiudad();
         var ventanaViaje = new VentanaViaje();
         ventanaCiudad.setVisible(true);
         ventanaViaje.setVisible(true);
         this.jDesktopPane1.add(ventanaCiudad);
-        this.jDesktopPane1.add(ventanaViaje);
+        this.jDesktopPane1.add(ventanaViaje);*/
 
     }
 
@@ -34,19 +37,49 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     private void initComponents() {
 
         jDesktopPane1 = new javax.swing.JDesktopPane();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(800, 600));
 
         javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
         jDesktopPane1.setLayout(jDesktopPane1Layout);
         jDesktopPane1Layout.setHorizontalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 388, Short.MAX_VALUE)
+            .addGap(0, 710, Short.MAX_VALUE)
         );
         jDesktopPane1Layout.setVerticalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 294, Short.MAX_VALUE)
+            .addGap(0, 427, Short.MAX_VALUE)
         );
+
+        jMenu2.setText("Ciudad");
+
+        jMenuItem1.setText("Crear");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jMenuItem1);
+
+        jMenuItem2.setText("Modificar");
+        jMenu2.add(jMenuItem2);
+
+        jMenuItem3.setText("Eliminar");
+        jMenu2.add(jMenuItem3);
+
+        jMenuItem4.setText("Listar");
+        jMenu2.add(jMenuItem4);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -65,6 +98,17 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+        
+        System.out.println("Hola Mundo");
+        var ventanaCiudad= new VentanaCiudad();
+        ventanaCiudad.setClosable(true);
+        ventanaCiudad.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        ventanaCiudad.setVisible(true);
+        
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -103,5 +147,11 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
     // End of variables declaration//GEN-END:variables
 }
